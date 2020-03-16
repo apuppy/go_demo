32ccbd4f81de4e60a88a9865e51122b8bd37a2c5
lihongde <lihongde@leweikeji.com>
2020-03-16 23:44:34 +0800

    urlshort program

---
+++ src/urlshort/handler/handler.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"net/http"
+
+	yamlV2 "gopkg.in/yaml.v2"
+)
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path, ok := pathsToUrls[r.URL.Path]
+		if ok {
+			http.Redirect(w, r, path, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+}
+
+func YamlHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsedYaml, err := parseYaml(yaml)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYaml(yaml []byte) (dst []map[string]string, err error) {
+	err = yamlV2.Unmarshal(yaml, &dst)
+	return dst, err
+}
+
+func buildMap(parsedYaml []map[string]string) map[string]string {
+	mergedMap := make(map[string]string)
+	for _, entry := range parsedYaml {
+		key := entry["path"]
+		mergedMap[key] = entry["url"]
+	}
+	return mergedMap
+}
