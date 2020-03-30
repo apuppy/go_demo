3dc0fcdf653ea59529de8ddb6afe95ca46b725f4
lihongde <lihongde@leweikeji.com>
2020-03-30 11:40:52 +0800

    http CORS

--- src/gorm-demo/api/server.go
+++ src/gorm-demo/api/server.go
@@ -5,11 +5,13 @@ import (
 	"gorm-demo/api/route"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/handlers"
 )
 
 func listen(p int) {
 	fmt.Printf("\n\nListening port %d...\n\n", p)
 	port := fmt.Sprintf(":%d", p)
 	r := route.NewRouter()
-	log.Fatal(http.ListenAndServe(port, r))
+	log.Fatal(http.ListenAndServe(port, handlers.CORS()(r)))
 }
