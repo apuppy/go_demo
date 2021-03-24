886e71e4d450175083497cffb16d0ec360992556
lihongde <chinalihongde@163.com>
2021-03-24 17:46:23 +0800

    context and panic recover test

---
+++ src/training/context_demo.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"github.com/google/uuid"
+)
+
+func main() {
+	helloWorldHandler := http.HandlerFunc(HelloWorld)
+	http.Handle("/welcome", injectMsgID(helloWorldHandler))
+	http.ListenAndServe(":8088", nil)
+}
+
+func HelloWorld (w http.ResponseWriter, r *http.Request) {
+	msgID := ""
+	if m := r.Context().Value("msgId"); m != nil {
+		if value, ok := m.(string); ok {
+			msgID = value
+		}
+	}
+	w.Header().Add("request-id", msgID)
+	w.Write([]byte("Hello, world."))
+}
+
+func injectMsgID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		msgID := uuid.New().String()
+		ctx := context.WithValue(r.Context(), "msgId", msgID)
+		req := r.WithContext(ctx)
+		next.ServeHTTP(w, req)
+	})
+}
---
+++ src/training/panic_recover.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	defer func(){
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+	fmt.Println("hahaha!!!")
+	ding()
+	panic("error defined by myself in main...")
+}
+
+func ding() {
+	ping()
+	panic("error from ding...")
+}
+
+func ping() {
+	panic("error from ping...")
+}
