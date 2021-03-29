25c84cd229a4f5a021e97e2f64f192f2b7a9d652
lihongde <chinalihongde@163.com>
2021-03-29 21:06:50 +0800

    Using Context Package, WithValue,WithCancel,WithTimeout,WithDeadline

--- src/training/context_demo_with_value.go
+++ src/training/context_demo_with_value.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
-	"net/http"
 	"github.com/google/uuid"
+	"net/http"
 )
 
 func main() {
@@ -12,7 +12,7 @@ func main() {
 	http.ListenAndServe(":8088", nil)
 }
 
-func HelloWorld (w http.ResponseWriter, r *http.Request) {
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	msgID := ""
 	if m := r.Context().Value("msgId"); m != nil {
 		if value, ok := m.(string); ok {
@@ -24,7 +24,7 @@ func HelloWorld (w http.ResponseWriter, r *http.Request) {
 }
 
 func injectMsgID(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		msgID := uuid.New().String()
 		ctx := context.WithValue(r.Context(), "msgId", msgID)
 		req := r.WithContext(ctx)
